506652f19482c94001e0b85a77a46bec820fc4ac
azubuikestanley <azubuikestanley@gmail.com>
2019-11-17 17:54:57 +0100

    pagination added

--- client/src/components/Movielist.js
+++ client/src/components/Movielist.js
@@ -1,57 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Pagination from './Pagination';
 import { connect } from 'react-redux';
 import { fetchMovie } from '../redux/actions/moviesActions';
 import PropTypes from 'prop-types';
+import Movies from './Movies';
 import '../SassMovie.scss';
 
 
 const Movielist = ({fetchMovie, movies}) => {    
-   
+//    const [posts, setPosts] = useState([]);
+   const [loading, setLoading] = useState(false);
+   const [currentPage, setCurrentPage] = useState(1);
+   const  [postsPerPage] = useState(12);
 
   useEffect(() => {
-      fetchMovie()
+      setLoading(true);
+      fetchMovie();
+      setLoading(false);
       return () => {
           
       };
   }, []);
+console.log(movies.movies);
 
+    // Get Current Movies
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const currentPosts = movies.movies.slice(indexOfFirstPost, indexOfLastPost);
+
+    // Change page
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
     return (
         <div className="mt-3">
             <div className="container-fluid">
-                <div className="row">
-               
-                {movies.movies.map((movie) => (
-                    <div className="col-xl-3 col-lg-3 col-md-6 col-sm-12" key={movie.id}>
-                        <div className="movie-content">
-                            <div className="movie-wrapper">
-                                <div className="movie-picture">
-                                    <img src={movie.image.medium} alt="movie-img" />
-                                </div>
-                            </div>
-                            <div className="movie-content-inner">
-                                <h6 className="movie-title">{movie.name}</h6> 
-                                <p>Rating: {movie.rating.average} <i className="material-icons">stars</i></p>
-                                <p><span>Year: </span> {movie.premiered}</p>                             
-                                <p><span>Language: </span>{movie.language}</p>  
-                                <a 
-                                    href={`/moviedetail/${movie.id}`} 
-                                    className="btn btn-primary d-flex justify-content-center"
-                                >
-                                    View Details
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                ))};
-
-                </div>
-            </div>
- 
-            <div>
-                <Pagination />
+                <Movies movies={currentPosts} loading={loading} />
+                <br />
+                <Pagination 
+                    postsPerPage={postsPerPage} 
+                    totalPosts={movies.movies.length}
+                    paginate={paginate}
+                />
             </div>
         </div>
     );
---
+++ client/src/components/Movies.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Movies = ({ movies, loading }) => {
+ if (loading) {
+     return <h2>Loading...</h2>;
+ }
+    return (
+        <div className="row">
+            {movies.map((movie) => (
+                <div className="col-xl-3 col-lg-3 col-md-6 col-sm-12" key={movie.id}>
+                    <div className="movie-content">
+                        <div className="movie-wrapper">
+                            <div className="movie-picture">
+                                <img src={movie.image.medium} alt="movie-img" />
+                            </div>
+                        </div>
+                        <div className="movie-content-inner">
+                            <h6 className="movie-title">{movie.name}</h6> 
+                            <p>Rating: {movie.rating.average} <i className="material-icons">stars</i></p>
+                            <p><span>Year: </span> {movie.premiered}</p>                             
+                            <p><span>Language: </span>{movie.language}</p>  
+                            <a 
+                                href={`/moviedetail/${movie.id}`} 
+                                className="btn btn-primary d-flex justify-content-center"
+                            >
+                                View Details
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default Movies;
